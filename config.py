2748271fa38a017e0aa2804451ba3d97e9c3c426
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2024-10-22 10:53:35 -0500

    Use yaml.safe_load() directly and add error handling

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import yaml
-from yaml.loader import SafeLoader
 
-relay_config = {}
-with open("config.yaml", "r") as f:
-    relay_config = yaml.load(f, Loader=SafeLoader)
+try:
+    with open("config.yaml", "r") as f:
+        relay_config = yaml.safe_load(f)
+except FileNotFoundError:
+    raise FileNotFoundError("Configuration file 'config.yaml' not found.")
+except yaml.YAMLError as e:
+    raise ValueError(f"Error parsing 'config.yaml': {e}")
