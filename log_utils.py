28667a2805d9a13477c6b74f9d4e2f666e23f7d6
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2024-10-22 11:08:43 -0500

    Added option for logging to file

--- log_utils.py
+++ log_utils.py
@@ -3,18 +3,21 @@ from config import relay_config
 
 
 def get_logger(name):
-    logger = logging.getLogger(name=name)
-    log_level = getattr(logging, relay_config["logging"]["level"].upper())
-
-    logger.setLevel(log_level)
-    logger.propagate = False  # Add this line to prevent double logging
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(
-        logging.Formatter(
-            fmt=f"%(asctime)s %(levelname)s:%(name)s:%(message)s",
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        log_level = getattr(logging, relay_config["logging"]["level"].upper(), logging.INFO)
+        logger.setLevel(log_level)
+        formatter = logging.Formatter(
+            fmt="%(asctime)s %(levelname)s:%(name)s:%(message)s",
             datefmt="%Y-%m-%d %H:%M:%S %z",
         )
-    )
-    logger.addHandler(handler)
+        # Stream Handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+        # File Handler
+        if relay_config["logging"].get("file"):
+            file_handler = logging.FileHandler(relay_config["logging"]["file"])
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
     return logger
