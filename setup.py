337df36f87a9923edd8099e46fb58bfbe64f9cc9
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2025-09-27 17:27:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ],
     python_requires=">=3.9",
     install_requires=[
-        "meshtastic>=2.6.4",
+        "meshtastic @ git+https://github.com/jeremiah-k/meshtastic-python.git@fix/ble-interface-hang",
         "Pillow==11.3.0",
         "matrix-nio==0.25.2",
         "matplotlib==3.10.1",
