d5c14b6e9fff9cbbc879337b503f503682134610
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2025-11-28 07:08:24 -0600

    Update meshtastic dependency to fork commit ble-1118-1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
     python_requires=">=3.10",
     install_requires=[
-        "meshtastic>=2.7.4",
+        "meshtastic @ git+https://github.com/jeremiah-k/meshtastic-python.git@ble-1118-1",
         "Pillow==12.0.0",
         "matrix-nio==0.25.2",
         "matplotlib==3.10.7",
