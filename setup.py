a12aae2c3039e5b2e8a684ab1a009626ec16638a
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2026-02-12 15:14:25 -0600

    build(deps): update meshtastic dependency to ble-2026-dev-1 branch

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
     python_requires=">=3.10",
     install_requires=[
-        "meshtastic>=2.7.4",
+        "meshtastic @ git+https://github.com/jeremiah-k/meshtastic-python.git@ble-2026-dev-1",
         "Pillow==12.1.0",
         "matrix-nio==0.25.2",
         "matplotlib==3.10.8",
