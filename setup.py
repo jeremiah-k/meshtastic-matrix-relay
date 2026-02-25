88894656318becb1ea93d25089e3e4eda87739c2
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2026-02-25 14:37:54 -0600

    build(deps): update meshtastic dependency to refactor-2026-v3-1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     ],
     python_requires=">=3.10",
     install_requires=[
-        "meshtastic @ git+https://github.com/jeremiah-k/meshtastic-python.git@refactor-2026-v2-1",
+        "meshtastic @ git+https://github.com/jeremiah-k/meshtastic-python.git@refactor-2026-v3-1",
         "Pillow==12.1.1",
         "matrix-nio==0.25.2",
         "matplotlib==3.10.8",
